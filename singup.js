133a149fc5efd277d9e98d744c2d9aa179e53b69
Sakthiajith <127951942+Sakthiajith@users.noreply.github.com>
2023-03-23 18:13:56 +0530

    Add files via upload

---
+++ singup.js
@@ -0,0 +1,14 @@
+function checkMail(){
+
+    if(email.value.match(/^[A-Za-z\._\-0-9]*[@][a-z]*[\.][a-z]{2,4}$/) && psw.value.match(/^[A-Za-z\-0-9]{1,20}$/))
+    {
+        window.confirm("welcome!!!");
+        window.location.href='file:///D:/html/spotify/index.html';
+        return true;
+    }
+    else{
+        window.confirm("check your email or password");
+        return false;
+        
+    }   
+}
