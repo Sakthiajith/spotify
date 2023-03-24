81943e39d14aa5df3e19c6685cddd7ac4ddf3c8e
Sakthiajith <127951942+Sakthiajith@users.noreply.github.com>
2023-03-24 14:28:27 +0530

    Update singup.js

--- singup.js
+++ singup.js
@@ -3,7 +3,7 @@ function checkMail(){
     if(email.value.match(/^[A-Za-z\._\-0-9]*[@][a-z]*[\.][a-z]{2,4}$/) && psw.value.match(/^[A-Za-z\-0-9]{1,20}$/))
     {
         window.confirm("welcome!!!");
-        window.location.href='file:///D:/html/spotify/index.html';
+        window.location.href='index.html';
         return true;
     }
     else{
